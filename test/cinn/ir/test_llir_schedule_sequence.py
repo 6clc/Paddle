45f528f7e1caf4bed008ee305aa4b5b58c9fc36c
6clc <chaoliu.lc@foxmail.com>
2023-09-06 14:38:40 +0800

    test(CINN-LLIR): simple ut of reorder and split

--- test/cinn/ir/test_llir_schedule_sequence.py
+++ test/cinn/ir/test_llir_schedule_sequence.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from test.cinn.utils.testing import assert_llir_equal
+
 import cinn.schedule as sch
 from cinn import to_cinn_llir
 from cinn.runtime.data_array import DataArray
@@ -36,7 +38,26 @@ def test_split_reorder_elementwise():
                     k1 = k
                     Z[i1, j1] = Z[i1, j1] + X[i1, k] * Y[k, j1]
 
-    print(split_reorder_elementwise)
+    @to_cinn_llir
+    def split_reorder_elementwise_gt(
+        X: DataArray((1024, 1024)),
+        Y: DataArray((1024, 1024)),
+        Z: DataArray((1024, 1024)),
+    ):
+        for i_1 in range(64):
+            for i_0 in range(4):
+                for i in range(2):
+                    for i_2 in range(2):
+                        for j in range(1024):
+                            for k in range(1024):
+                                i1 = (512 * i) + (
+                                    (128 * i_0) + ((2 * i_1) + i_2)
+                                )
+                                j1 = j
+                                k1 = k
+                                Z[i1, j1] = Z[i1, j1] + (X[i1, k] * Y[k, j1])
+
+    assert_llir_equal(split_reorder_elementwise, split_reorder_elementwise_gt)
 
 
 if __name__ == "__main__":
